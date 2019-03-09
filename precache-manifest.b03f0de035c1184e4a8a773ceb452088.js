3c582047f696c2d9e43b952a7b0f88a3026e40f4
Seven <seven.chi@huafu.com.tw>
2019-03-09 17:16:12 +0800

    update: update the web site data

---
+++ precache-manifest.b03f0de035c1184e4a8a773ceb452088.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb467b265aae05a6425cdb9c55c4323b",
+    "url": "/seven.png"
+  },
+  {
+    "url": "/runtime~app.538599316f7f23883a0c.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f8a61a788cec92e07dc1f3098e260999",
+    "url": "/index.html"
+  },
+  {
+    "url": "/demo.538599316f7f23883a0c.js"
+  },
+  {
+    "revision": "2db4cdb35175592b5b09",
+    "url": "/css/demo.7cfe6833.css"
+  },
+  {
+    "revision": "b401926ce197f3cfef58",
+    "url": "/css/app.e308daf8.css"
+  },
+  {
+    "url": "/app.538599316f7f23883a0c.js"
+  }
+];
